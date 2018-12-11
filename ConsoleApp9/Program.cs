97502c675ef4ffaaed65b14479f13d4c05ad04d6
Jørgen Meedom <jmeedom@gmail.com>
2018-12-11 15:48:35 +0100

    Initial version

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        private MySqlConnection connection;
+
+        public void RunConnection()
+        {
+            string connectionString = "server=localhost;port=3306;database=mysql;uid=root;pwd=my-secret-pw";
+            using (connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                MySqlCommand myCommand = new MySqlCommand("CREATE TABLE jme22 (f1 char(255))");
+                myCommand.Connection = connection;
+                myCommand.ExecuteNonQuery();
+                myCommand.Dispose();
+                connection.Close();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.RunConnection();
+        }
+    }
+}
